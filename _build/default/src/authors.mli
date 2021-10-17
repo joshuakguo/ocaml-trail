47674a2c8e42ad4dd9b3a628a7f876c9e915b652
Joshua Guo <joshuakguo@gmail.com>
2021-10-16 20:50:41 -0400

    switching to dune
     - not working yet

---
+++ _build/default/src/authors.mli
@@ -0,0 +1,9 @@
+(** CS 3110 Fall 2021 Final Project
+
+    @author Michelle Sun (mss455)
+    @author Hannah Lee (hel33)
+    @author Meng Ting Li (ml2368)
+    @author Joshua Guo (jcg294) *)
+
+val hours_worked : int
+(** [hours_worked] is the number of hours worked on this project. *)
